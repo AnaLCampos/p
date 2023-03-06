2a941938062c2fb0c1b6e463c8dac10eb19ab75a
AnaLCampos <125393053+AnaLCampos@users.noreply.github.com>
2023-03-06 15:29:06 -0300

    Add files via upload

---
+++ Atividades/atv3.py
@@ -0,0 +1,13 @@
+num = int(input("Digite um número inteiro: "))
+
+if num <= 1:
+    print("O número não é primo")
+
+for i in range(2, num//2+1):
+    if num % i == 0:
+        print("O número não é primo")
+        break
+
+else:
+    print("O número é primo")
+
